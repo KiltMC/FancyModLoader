9572714b00e4532ee170881cc18ca849be1f5094
BluSpring <bluspringindohq@gmail.com>
2026-06-06 01:06:07 +0800

    Fixed Forge Config API Port again

--- loader/src/main/java/net/neoforged/fml/ModContainer.java
+++ loader/src/main/java/net/neoforged/fml/ModContainer.java
@@ -149,7 +149,7 @@ public abstract class ModContainer {
      * 
      * @param e Event to accept
      */
-    public final <T extends Event & IModBusEvent> void acceptEvent(T e) {
+    public <T extends Event & IModBusEvent> void acceptEvent(T e) {
         IEventBus bus = getEventBus();
         if (bus == null) return;
 
